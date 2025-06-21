0548a12681aa2ecb3489f48b59b5a3bb02c2cff8
chlghrud <wasdrty1126@gmail.com>
2025-06-21 14:28:51 +0900

    first commit

---
+++ 2025-IT-Software-Solutions-for-Business-Gangwon-1st-project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2025_IT_Software_Solutions_for_Business_Gangwon_1st_project
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
